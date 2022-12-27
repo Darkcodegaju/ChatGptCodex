189051527d3c8f520775c5611a8038bd7ca88e67
Darkcodegaju <93965561+Darkcodegaju@users.noreply.github.com>
2022-12-27 20:22:21 +0530

    Update script.jsx

--- client/script.jsx
+++ client/script.jsx
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codexgpt-rmak.onrender.com/', {
+    const response = await fetch('https://codeexxgpt.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
